bb3c7d3f78d7128799df14380a1c911fcb57eeba
kevinlampe1998 <kevin.lampe@dci-student.org>
2024-08-04 23:55:46 +0200

    first-version done

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+function App() {
+
+  return (
+    <main>
+      <div className="description1">
+        <div></div>
+        <h1>Take Rest in the Nature</h1>
+        <h2>Perfect Locations to get new Energy</h2>
+      </div>
+
+      <div className="img1"></div>
+
+      <div className="description2">
+        <p>Nature can refer to the general realm of living beings, and in some cases to the processes associated with inanimate objects—the way that particular types of things exist and change of their own accord, such as the weather and geology of the Earth. It is often taken to mean the "natural environment" or wilderness—wild animals, rocks, forest, and in general those things that have not been substantially altered by human intervention, or which persist despite human intervention.</p>
+        <p>With the Industrial Revolution, nature increasingly became seen as the part of reality deprived from intentional intervention: it was hence considered as sacred by some traditions (Rousseau, American transcendentalism) or a mere decorum for divine providence or human history (Hegel, Marx). However, a vitalist vision of nature, closer to the pre-Socratic one, got reborn at the same time, especially after Charles Darwin.[</p>
+        <p>This more traditional concept of natural things that can still be found today implies a distinction between the natural and the artificial, with the artificial being understood as that which has been brought into being by a human consciousness or a human mind. Depending on the particular context, the term "natural" might also be distinguished from the unnatural or the supernatural.</p>
+      </div>
+
+      <div className="img2"></div>
+
+      <div className="description3">
+        <p>Nature can refer to the general realm of living beings, and in some cases to the processes associated with inanimate objects—the way that particular types of things exist and change of their own accord, such as the weather and geology of the Earth. It is often taken to mean the "natural environment" or wilderness—wild animals, rocks, forest, and in general those things that have not been substantially altered by human intervention, or which persist despite human intervention.</p>
+        <p>With the Industrial Revolution, nature increasingly became seen as the part of reality deprived from intentional intervention: it was hence considered as sacred by some traditions (Rousseau, American transcendentalism) or a mere decorum for divine providence or human history (Hegel, Marx). However, a vitalist vision of nature, closer to the pre-Socratic one, got reborn at the same time, especially after Charles Darwin.[</p>
+        <p>This more traditional concept of natural things that can still be found today implies a distinction between the natural and the artificial, with the artificial being understood as that which has been brought into being by a human consciousness or a human mind. Depending on the particular context, the term "natural" might also be distinguished from the unnatural or the supernatural.</p>
+      </div>
+
+      <div className="img3"></div>
+      <div className="description4">
+        <h6>About</h6>
+        <h6>Contact</h6>
+        <h6>More</h6>
+      </div>
+    </main>
+  )
+}
+
+export default App
